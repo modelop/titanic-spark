0dbe2be432285e4df8198e29e72592720bae210c
mikelemikelo <mikelemikelo@gmail.com>
2021-01-12 13:56:13 -0700

    Adding file that will contain Job Metadata to be used inside the job

--- ModelOpPySparkDriver.py
+++ ModelOpPySparkDriver.py
@@ -1,38 +1,24 @@
+########################################################################################
+##### dynamic model / resources content.
+########################################################################################
+
+# primary source code
 import titanic_assets_access as modelop_model
+# job metadata, variables pointing to [input, output, appname,etc]
+import PySparkModelMetadata_titanic as modelop_job_metadata
+
+
+########################################################################################
+##### static resources content.
+########################################################################################
+
+
 
 if __name__ == "__main__":
     print("ModelOpPySparkExecution execution")
-    #input
-    #/hadoop/test.csv"
-    #model
-    #/hadoop/titanic
-    #output
-    #/hadoop/titanic_output.csv
 
-    job_input = {
-         "test.csv":{
-             "assetType": "EXTERNAL_FILE",
-             "fileUrl": "/hadoop/test.csv",
-             "filename": "test.csv",
-             "fileFormat": "CSV"
-         },
-         "titanic":{
-             "assetType": "EXTERNAL_FILE",
-             "fileUrl": "/hadoop/titanic",
-             "filename": "titanic"
-         }
-         }
-
-    job_output = {
-        "titanic_output.csv":{
-           "assetType": "EXTERNAL_FILE",
-           "fileUrl": "/hadoop/titanic_output.csv",
-           "filename": "titanic_output.csv",
-           "fileFormat": "CSV"
-            }
-        }
 
     ## Executing requested function
-    modelop_model.score(job_input, job_output)
+    modelop_model.score(modelop_job_metadata.job_input, modelop_job_metadata.job_output)
 
     print("PySpark code execution completed")
