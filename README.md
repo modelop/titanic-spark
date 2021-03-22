e96c338aa7d2bf12239ee8082d96d5cd16d5dde4
Nicky Marino <nickymarino@icloud.com>
2021-03-22 15:57:21 -0500

    Update model for new Spark args and external asset locations

--- README.md
+++ README.md
@@ -1,12 +1,50 @@
-# spark-example
+# titanic-spark
 
-To run this example:
-* Follow the Spark container setup here: https://modelop.atlassian.net/wiki/spaces/VDP/pages/1283293189/Spark+Integration
-* Unzip titanic.zip
-* Run `scp -r LAPTOP_USERNAME@host.docker.internal:/Users/LAPTOP_USERNAME/path/to/repo/titanic/ .` from the container.
-* Run `scp LAPTOP_USERNAME@host.docker.internal:/Users/LAPTOP_USERNAME/path/to/repo/test.csv` also from the container.
-* Run `hadoop fs -mkdir -p /hadoop/` to specify the path in hdfs also from the container.
-* Run `hadoop fs -put titanic/ /hadoop/titanic/` to put the model object in hdfs.
-* Run `hadoop fs -put test.csv /hadoop/test.csv` so the test data is available.
-* Run `./spark-submit --master yarn --executor-memory 512MB --total-executor-cores 10 /Users/LAPTOP_USERNAME/path/to/repo/titanic.py`
-* There should be an hdfs CSV file name titanic_output.csv in the container. (`hadoop fs -ls /hadoop/` should list all files)
+This repo is an example Spark model that is conformed for use with ModelOp Center and the ModelOp Spark Runtime Service.
+
+## Assets
+
+There are three assets that are used to run this example:
+
+| Asset Type | Repo File | HDFS Path | Description |
+| --- | --- | --- | --- |
+| Model Binary | `./titanic.zip` | `/hadoop/demo/titanic-spark/titanic` | Spark model binary compressed as a zip file in this repo, but must be expanded and be available in the Spark cluster HDFS for the model's `init()` function to run |
+| Input Asset | `./test.csv` | `/hadoop/demo/titanic-spark/test.csv` | Input file for the model `score()` function. The HDFS path can vary based on the `external_inputs` param of the `score()` function  |
+| Output Asset | n/a | `/hadoop/demo/titanic-spark/titanic_output.csv` | Output file from the model `score()` function. The HDFS path can vary based on the `external_outputs` param of the `score()` function  |
+
+## Mocaasin Tests
+
+It is recommended to use the image `modelop/spark-cluster:dev-bp-ds` for the Spark cluster during testing because it includes libraries and assets that are needed to run this model.
+
+1. Verify that the model binary (above) exists at `/hadoop/demo/titanic-spark/titanic` in the Spark cluster HDFS and verify that the input asset exists at `/hadoop/demo/titanic-spark/test.csv`. The input asset can be in a different location, but you must update the input asset URL in step (3) below.
+2. Import this repository to ModelOp Center
+3. Create a new scoring job with the following HDFS URL assets (do not select the "Secured" option):
+  - Input asset: `hdfs:///hadoop/demo/titanic-spark/test.csv`
+  - Output asset: `hdfs:///hadoop/demo/titanic-spark/titanic_output.csv`
+4. Wait for the job to enter the `COMPLETE` state
+5. Inside the Spark cluter, use `hadoop fs -getmerge -nl /hadoop/demo/titanic-spark/titanic_output.csv /home/cloudera/titanic_output.csv` and `kubectl cp SPARK_CLUSTER_POD_NAME:/home/cloudera/titanic_output.csv titanic_output.csv` to copy the output asset to your local computer.
+  - After running `hadoop fs -getmerge ...`, you can run `cat titanic_output.csv` in the Spark cluster and you should see the following printed to the terminal:
+
+```
+Pclass,Age,Gender,Fare,Boarded,prediction
+3.0,26.0,0.0,7.8958,0.0,0.0
+1.0,25.0,1.0,151.55,0.0,1.0
+3.0,40.0,0.0,7.225,1.0,0.0
+3.0,1.0,1.0,11.1333,0.0,1.0
+3.0,11.0,0.0,46.9,0.0,0.0
+2.0,36.0,1.0,26.0,0.0,1.0
+...
+```
+
+## Local Tests
+
+To run this example locally:
+- Follow the Spark container setup here: https://modelop.atlassian.net/wiki/spaces/VDP/pages/1283293189/Spark+Integration
+- Unzip titanic.zip
+- Run `scp -r LAPTOP_USERNAME@host.docker.internal:/Users/LAPTOP_USERNAME/path/to/repo/titanic/ .` from the container.
+- Run `scp LAPTOP_USERNAME@host.docker.internal:/Users/LAPTOP_USERNAME/path/to/repo/test.csv` also from the container.
+- Run `hadoop fs -mkdir -p /hadoop/` to specify the path in hdfs also from the container.
+- Run `hadoop fs -put titanic/ /hadoop/titanic/` to put the model object in hdfs.
+- Run `hadoop fs -put test.csv /hadoop/test.csv` so the test data is available.
+- Run `./spark-submit --master yarn --executor-memory 512MB --total-executor-cores 10 /Users/LAPTOP_USERNAME/path/to/repo/titanic.py`
+- There should be an hdfs CSV file name titanic_output.csv in the container. (`hadoop fs -ls /hadoop/` should list all files)
--- titanic.py
+++ titanic.py
@@ -13,68 +13,95 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import RandomForestClassificationModel
 
-#modelop.score
-def score(input_file_loc, output_file_loc):
-    df = (spark.read
-          .format("csv")
-          .option('header', 'true')
-          .load(input_file_loc))
 
-    dataset = df.select(col('Pclass').cast('float'),
-                        col('Sex'),
-                        col('Age').cast('float'),
-                        col('Fare').cast('float'),
-                        col('Embarked')
-                        )
+# modelop.init
+def init():
+    print("Begin function...")
 
-    dataset = dataset.replace('?', None)\
-        .dropna(how='any')
+    global SPARK
+    SPARK = SparkSession.builder.appName("DriftTest").getOrCreate()
 
-    dataset = StringIndexer(
-        inputCol='Sex', 
-        outputCol='Gender', 
-        handleInvalid='keep').fit(dataset).transform(dataset)
+    global MODEL
+    MODEL = RandomForestClassificationModel.load("/hadoop/demo/titanic-spark/titanic")
 
-    dataset = StringIndexer(
-        inputCol='Embarked', 
-        outputCol='Boarded', 
-        handleInvalid='keep').fit(dataset).transform(dataset)
 
-    dataset = dataset.drop('Sex')
-    dataset = dataset.drop('Embarked')
+# modelop.score
+def score(external_inputs, external_outputs, external_model_assets):
+    # Grab single input asset and single output asset file paths
+    input_asset_path, output_asset_path = parse_assets(
+        external_inputs, external_outputs
+    )
 
-    required_features = ['Pclass',
-                    'Age',
-                    'Fare',
-                    'Gender',
-                    'Boarded'
-                   ]
+    df = SPARK.read.format("csv").option("header", "true").load(input_asset_path)
 
-    assembler = VectorAssembler(inputCols=required_features, outputCol='features')
+    dataset = df.select(
+        col("Pclass").cast("float"),
+        col("Sex"),
+        col("Age").cast("float"),
+        col("Fare").cast("float"),
+        col("Embarked"),
+    )
+
+    dataset = dataset.replace("?", None).dropna(how="any")
+
+    dataset = (
+        StringIndexer(inputCol="Sex", outputCol="Gender", handleInvalid="keep")
+        .fit(dataset)
+        .transform(dataset)
+    )
+
+    dataset = (
+        StringIndexer(inputCol="Embarked", outputCol="Boarded", handleInvalid="keep")
+        .fit(dataset)
+        .transform(dataset)
+    )
+
+    dataset = dataset.drop("Sex")
+    dataset = dataset.drop("Embarked")
+
+    required_features = ["Pclass", "Age", "Fare", "Gender", "Boarded"]
+
+    assembler = VectorAssembler(inputCols=required_features, outputCol="features")
     transformed_data = assembler.transform(dataset)
 
-    model = RandomForestClassificationModel.load('/hadoop/titanic')
-
-    predictions = model.transform(transformed_data)
+    predictions = MODEL.transform(transformed_data)
     get_propensity = udf(lambda x: x[1], ArrayType(FloatType()))
     print(predictions.head(5))
-    predictions = predictions.select('Pclass',
-                        'Age',
-                        'Gender',
-                        'Fare',
-                        'Boarded',
-                        'prediction'
-                        )
+    predictions = predictions.select(
+        "Pclass", "Age", "Gender", "Fare", "Boarded", "prediction"
+    )
 
     print(predictions.head(5))
-    predictions.write.csv(output_file_loc)
-    return 
+    # Use coalesce() so that the output CSV is a single file for easy reading
+    predictions.coalesce(1).write.mode("overwrite").option("header", "true").csv(
+        output_asset_path
+    )
 
-if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("Titanic")\
-        .getOrCreate()
+    SPARK.stop()
 
-    score('/hadoop/test.csv', '/hadoop/titanic_output.csv')
-    spark.stop()
+
+def parse_assets(external_inputs, external_outputs):
+    """Returns a tuple (input asset hdfs path, output asset hdfs path)"""
+
+    # Fail if more assets than expected
+    if len(external_inputs) != 1:
+        raise ValueError("Only one input asset should be provided")
+    if len(external_outputs) != 1:
+        raise ValueError("Only one output asset should be provided")
+
+    # There's only one key-value pair in each dict, so
+    # grab the first value from both
+    input_asset = list(external_inputs.values())[0]
+    output_asset = list(external_outputs.values())[0]
+
+    # Fail if assets are JSON
+    if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
+        raise ValueError("Input file format is set as JSON but must be CSV")
+    if ("fileFormat" in output_asset) and (output_asset["fileFormat"] == "JSON"):
+        raise ValueError("Output file format is set as JSON but must be CSV")
+
+    # Return paths from file URLs
+    input_asset_path = input_asset["fileUrl"]
+    output_asset_path = output_asset["fileUrl"]
+
+    return (input_asset_path, output_asset_path)
